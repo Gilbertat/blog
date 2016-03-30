aa5f9c518dcdacc602dffe2840f651ca8c6bff60
Gilbertat <shiyue45457@gmail.com>
2016-03-30 16:39:16 +0800

    user markdown

--- models/post.js
+++ models/post.js
@@ -1,5 +1,6 @@
 // 引入数据库
 var mongodb = require('./db');
+var markdown = require('markdown').markdown;
 
 function Post(name, title, post) {
     this.name = name;
@@ -77,6 +78,10 @@ Post.get = function (name, callback) {
                 if (err) {
                     return callback(err);
                 }
+                // 解析markdown 为html
+                docs.forEach(function (doc) {
+                    doc.post = markdown.toHTML(doc.post);
+                });
                 callback(null, docs);
             });
         });
